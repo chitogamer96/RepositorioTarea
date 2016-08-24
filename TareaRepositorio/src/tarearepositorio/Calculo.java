7765518ed90509d074a2cb002b197745cf70be77
PC 10 <PC 10@DESKTOP-GJQCDQJ>
2016-08-24 13:30:24 -0500

    Segundo Commit

---
+++ TareaRepositorio/src/tarearepositorio/Calculo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarearepositorio;
+
+/**
+ *
+ * @author PC 10
+ */
+public class Calculo {
+    public int CalcularHoras(int hor)
+    {
+        int dineroxhora=5;
+    
+        return hor*dineroxhora;
+    }
+    
+}
