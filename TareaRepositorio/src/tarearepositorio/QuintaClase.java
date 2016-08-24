c63cfb787e2c7cf81c9739c6b4335ac50904c620
PC 10 <PC 10@DESKTOP-GJQCDQJ>
2016-08-24 13:36:11 -0500

    Quinto Commit

---
+++ TareaRepositorio/src/tarearepositorio/QuintaClase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarearepositorio;
+
+/**
+ *
+ * @author PC 10
+ */
+public class QuintaClase {
+    
+    
+    
+    
+ public void Nombre()
+ {
+     System.out.println("Este es el ultimo metodos :)");
+ }
+}
